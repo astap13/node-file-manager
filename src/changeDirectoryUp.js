630cfc3d3bfc622b76c343c26d39afa49fba4a37
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-14 01:20:55 +0200

    refactor: add rel path to up

--- src/changeDirectoryUp.js
+++ src/changeDirectoryUp.js
@@ -2,11 +2,25 @@ import { displayCurrentDirectory } from "../src/utils/displayCurrentDirectory.js
 import { dirname } from "path";
 import { currentDir } from "../src/utils/displayCurrentDirectory.js";
 import { changeCurrentDirectory } from "../src/utils/displayCurrentDirectory.js";
+import { existsSync, statSync } from "fs";
 
 export function changeDirectoryUp() {
   const parentDir = dirname(currentDir);
-  if (parentDir !== currentDir) {
-    changeCurrentDirectory(parentDir);
+
+  if (!existsSync(parentDir)) {
+    console.log(
+      `Operation failed: parent directory "${parentDir}" does not exist.`
+    );
+    return;
   }
+
+  if (!statSync(parentDir).isDirectory()) {
+    console.log(
+      `Operation failed: path "${parentDir}" exists but is not a directory.`
+    );
+    return;
+  }
+
+  changeCurrentDirectory(parentDir);
   displayCurrentDirectory();
 }
