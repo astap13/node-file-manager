1452c16769653fe41d9958ef830e14a10f77e82f
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-14 01:19:12 +0200

    refactor: add path

--- src/changeDirectory.js
+++ src/changeDirectory.js
@@ -4,26 +4,22 @@ import { currentDir } from "./utils/displayCurrentDirectory.js";
 import { changeCurrentDirectory } from "./utils/displayCurrentDirectory.js";
 
 export function changeDirectory(newDir) {
-  if (!newDir || typeof newDir !== "string") {
-    console.log(
-      "Operation failed: new directory path is required and should be a string."
-    );
-    return;
-  }
+    if (!newDir || typeof newDir !== 'string') {
+        console.log('Operation failed: new directory path is required and should be a string.');
+        return;
+    }
 
-  const newPath = isAbsolute(newDir) ? newDir : join(currentDir, newDir);
+    const newPath = isAbsolute(newDir) ? newDir : join(currentDir, newDir);
 
-  if (!existsSync(newPath)) {
-    console.log(`Operation failed: directory "${newPath}" does not exist.`);
-    return;
-  }
+    if (!existsSync(newPath)) {
+        console.log(`Operation failed: directory "${newPath}" does not exist.`);
+        return;
+    }
 
-  if (!statSync(newPath).isDirectory()) {
-    console.log(
-      `Operation failed: path "${newPath}" exists but is not a directory.`
-    );
-    return;
-  }
+    if (!statSync(newPath).isDirectory()) {
+        console.log(`Operation failed: path "${newPath}" exists but is not a directory.`);
+        return;
+    }
 
-  changeCurrentDirectory(newPath);
+    changeCurrentDirectory(newPath);
 }
