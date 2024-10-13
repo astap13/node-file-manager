c9ae1ed448faab79dd149d1d68faabde60c7f378
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-13 02:25:15 +0200

    feat: exit app and handle unknown command

--- fileManager.js
+++ fileManager.js
@@ -1,4 +1,38 @@
+const os = require('os');
+
+const homeDir = os.homedir();
+let currentDir = homeDir;
+
+// Function to display the current working directory
+function displayCurrentDirectory() {
+    console.log(`You are currently in ${currentDir}`);
+}
+
+function handleUnknownCommand() {
+    console.log('Invalid input. Please try again.');
+}
+
+
+function handleCommand(command) {
+    const [cmd, ...args] = command.trim().split(' ');
+
+    switch (cmd) {
+        case '.exit':
+            console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+            process.exit();
+        default:
+            handleUnknownCommand();
+            break;
+    }
+}
+
+
+
 const username = process.argv.find(arg => arg.startsWith('--username=')).split('=')[1];
 
 console.log(`Welcome to the File Manager, ${username}!`);
 
+process.stdin.on('data', (input) => {
+    handleCommand(input.toString());
+    displayCurrentDirectory();
+});
